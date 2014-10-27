7c960234ef93d6f9e311cde05b1eeba1241bf369
Joshua Morgan <joshuamorganusa@gmail.com>
2014-10-26 21:54:36 -0400

    Fixed TF to include end points

--- Matlab/Get_TF.m
+++ Matlab/Get_TF.m
@@ -9,7 +9,7 @@ upper_bound_db = 3;
 normal_vs_client = xlsread('normal-vs-client_TOH.xls');
 
 tf = [normal_vs_client(1,:); normal_vs_client(4,:)];
-%tf = [[lower_bound_freq; lower_bound_db], tf, [upper_bound_freq; upper_bound_db]];
+tf = [[lower_bound_freq; lower_bound_db], tf, [upper_bound_freq; upper_bound_db]];
 
 f1 = figure(1)
 semilogx(tf(1,:), tf(2,:), 'k-', 'LineWidth', 2);
