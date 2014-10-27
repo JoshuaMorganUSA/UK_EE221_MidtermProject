aa1e4fc8b7df5b97106590edf414cfd9c6cf09fa
Joshua Morgan <joshuamorganusa@gmail.com>
2014-10-27 01:16:08 -0400

    Added file for testing BP filters

---
+++ Matlab/TF_test.m
@@ -0,0 +1,23 @@
+tf = xlsread('tf_gain.xls');
+close all
+
+f0 = 14000;
+Q = f0/3000
+dB = 12
+
+A = 10^(dB/20)
+freq_min = 10
+freq_max = 20000
+
+w0 = f0*2*pi;
+
+f = [freq_min:1:freq_max];
+s = f * 2 * pi * i;
+B = w0/Q;
+H = (A * s * B)./(w0^2 + s * B + s.^2);
+
+figure(1);
+semilogx(tf(1,:), tf(2,:), 'k*-', 'LineWidth', 2);
+hold on
+grid
+semilogx(f, 20*log10(abs(H)),'r-');
